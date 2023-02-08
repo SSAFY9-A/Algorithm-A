057052caabf26c136be5a88bdb6d198513778139
uubbn <112951227+uubbn@users.noreply.github.com>
2023-02-09 02:04:49 +0900

    Update week1_01프로세서 연결.cpp

--- Week01/yubin/week1_01프로세서 연결.cpp
+++ Week01/yubin/week1_01프로세서 연결.cpp
@@ -2,12 +2,15 @@
 
 using namespace std;
 
-int map[12][12];
-int N;
-int maxcore;
-int flag = 0;
-int CNT;
-int MIN = 21e8;
+int map[12][12]; // 프로세서가 담길 cell
+int N; //map 크기
+int maxcore; //core수 세기
+int CNT; //전선 수 세기
+int MIN ; //전선 수 의 최소
+int MAX ; //core 수 의 최대
+
+//check 함수 : 상하좌우 전선을 놓을 수 있는지 확인
+//d=0 상, d=1 하, d=2좌, d=3 우.
 int check(int y, int x, int d)
 {
 	//상,하,좌,우
@@ -29,89 +32,105 @@ int check(int y, int x, int d)
 	return 1;
 }
 
-int inoutMap(int y, int x, int d,int n)
+//inoutmap 함수 . map[y][x]에서 d방향으로 n값을 채워줌.
+//n 은 전선을 뜻하는 2와 이를 복구하기 위한 0이 들어감. 
+void inoutMap(int y, int x, int d,int n)
 {
-	int count = 0;
 	if (d == 0)
-		for (int i = 0; i < y; i++)
-		{
-			map[i][x] = n;
-			count++;
-		}
+		for (int i = 0; i < y; i++) map[i][x] = n;
+
 	if (d == 1)
-		for (int i = y + 1; i < N; i++)
-		{
-			map[i][x] = n;
-			count++;
-		}
+		for (int i = y + 1; i < N; i++) map[i][x] = n;
 
 	if (d == 2)
-		for (int i = 0; i < x; i++)
-		{
-			map[y][i] = n;
-			count++;
-		}
+		for (int i = 0; i < x; i++) map[y][i] = n;
+		
 
 	if (d == 3)
-		for (int i = x + 1; i < N; i++)
-		{
-			map[y][i] = n;
-			count++;
-		}
-	return count;
+		for (int i = x + 1; i < N; i++) map[y][i] = n;
+
 }
 
+//datamap 함수. 전선의 수를 미리 파악하기 위한 함수
+//예약개념. 해당하는 d 방향으로 채운다면? return 전선수
+int dataMap(int y, int x, int d)
+{
+	if (d == 0) return y;
+	if (d == 1) return(N - y - 1);
+
+	if (d == 2) return x;
+
+	if (d == 3) return (N - x - 1);	
+}
 
 void run(int level, int now)
 {
+	//return 조건
+	//level(행) 이 N-1인 경우. (N인경우 어차피 연결되므로 해당x)
 	if (level == N-1)
 	{
-		cout << "cnt" << CNT <<", min "<<MIN<< '\n';
-		cout << maxcore << ":max\n";
-		if (MIN > CNT)
+		//1. 만일 코어의 최대값이 같은 상태에서 전선이 최소라면 업데이트
+		if (maxcore==MAX && MIN > CNT)
 		{
 			MIN = CNT;
+			MAX = maxcore;
 		}
+		
+		//2. 만일 코어의 최대값이 증가한다면, 전선의 최소는 상관 없으므로 코어 최대로 업데이트
+		else if (maxcore > MAX)
+		{
+			MIN = CNT;
+			MAX = maxcore;
+		}
+
 		return;
 	}
-	//일단?! 한줄한줄 돌려야함. 
-	for (int i = now; i < N - 1; i++)
+
+	for (int i = now; i < N-1 ; i++)
 	{
 		if (map[level][i] !=1) continue;
-		//방향확인
-		flag = 0;
-		for (int j = 0; j < 4; j++)
+		for (int d = 0; d < 4; d++) //
 		{
-			if (check(level, i, j) == 0) continue;
-			int data = inoutMap(level, i, j, 2);
-			//체크하고?? map에 넣을거임
-			if (CNT + data > MIN) continue;
-			CNT+=data;
-			cout << level << "," << i << ") " << j << '\n';
-			//1.일단 다음 1로감
-			run(level,i+1);
-			CNT-=inoutMap(level, i, j, 0);
+			if (check(level, i, d) == 0) continue; //방향확인
+			int data = dataMap(level, i, d); //최솟값을 확인하기 위해 미리 전선의 수 파악
+
+			//return에서 업데이트 되지만 미리 가지치기(코어 최대보다 작은데 전선수가 큰 경우
+			if (CNT + data > MIN && maxcore+1<MAX) continue;  
 			
-			
-			//근데 다 안된다..??
-			flag = 1;
+			//전선 수 증가. 전선부분 2로 채움. 코어수 증가
+			CNT += data;
+			inoutMap(level, i, d, 2);
+			maxcore++;
+
+			//같은 행에 또 있을 수 있으니 level은 같게, x값만 증가하게 돌림.
+			run(level, i + 1);
+
+			maxcore--;
+			CNT -= data;
+			inoutMap(level, i, d, 0);
 		}
-		//flag=1이란 건 check에 걸리는 게 하나라도 있다는 얘기.core 수 ++ 해야함. 
-		//flag=0은 주변이 다 안된다. 
-		if (flag == 0) maxcore++;
 	}
-	//2.다음 1이 없으면 여기 
+	//해당level에 더 이상 코어가 없다면 행 증가. index는 1부터
 	run(level + 1,1);
-	
 }
 
 int main()
 {
-	cin >> N;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			cin >> map[i][j];
+	int tc;
+	cin >> tc;
+	for (int T = 0; T < tc; T++)
+	{
 
-	run(1,1);
-	cout << MIN;
+		CNT=0;
+		MIN = 21e8;
+		MAX = -1;
+		maxcore = 0;
+		cin >> N;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				cin >> map[i][j];
+
+		run(1, 1);
+		cout <<"#"<<T+1<<" "<< MIN<<'\n';
+	}
 }
