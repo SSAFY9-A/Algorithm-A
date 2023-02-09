033a848de19541dcf15f82f7264cd9f792325be2
uubbn <112951227+uubbn@users.noreply.github.com>
2023-02-10 02:19:46 +0900

    Update 등산로.cpp

--- Week01/yubin/[SWEA 1949]등산로.cpp
+++ Week01/yubin/[SWEA 1949]등산로.cpp
@@ -4,36 +4,54 @@
 
 using namespace std;
 
-int map[8][8];
-int flag;
-int MAX;
-int maxcnt;
+int map[8][8]; //부지
+int flag;  //깎았는지 flag로 확인 
+int MAX; //가장 높은 봉우리 값
+int maxcnt; // 등산로로 만들 수 있는 최대값
 int N, K;
+int CNT; //등산로를 세기 위한 count
+
 int dy[4] = { 1,-1,0,0 };
 int dx[4] = { 0,0,-1,1 };
+
 int via[8][8];
-int CNT;
+
+//run 함수 : 일단 방문 check 하고 감. 
 void run(int y, int x)
 {
 	via[y][x] = 1;
+	
 	for (int j = 0; j < 4; j++)
 	{
 		int cy = y + dy[j];
 		int cx = x + dx[j];
-
+		//해당 인덱스에서 상하좌우로 방향 확인.
+		//배열의 범위를 넘어가도 안되며 
 		if (cy < 0 || cx < 0 || cy >= N || cx >= N) continue;
+
+		//방문했던 index도 안되고
 		if (via[cy][cx] == 1) continue;
 
+		//K 만큼 깎을 수 있기때문에 . 
+		//다음에 올 위치와 지금의 차이가 K보다 커도 안되고 
 		if (map[cy][cx] - map[y][x] >= K) continue;
+
+		//flag=1이라면 값을 이미 바꿨다는 것을 뜻함. 
+		//다음에 올 위치가 현재위치보다 커도 되지만, flag=1이라면 안됨
 		if (map[cy][cx] >= map[y][x] && flag == 1) continue;
+
+		//flag도 0이고(깎지 않았고) 차이가 K이하라면 돌림
 		if (map[cy][cx]- map[y][x]>=0 && map[cy][cx] - map[y][x] < K)
 		{
-			
+			//최대K이므로 K까지 완전탐색
 			for (int k = 1; k <=K; k++)
 			{
-				
 				flag = 1;
+
+				//깎았는데도 크거나 같으면 안됨
 				if ((map[cy][cx]-k)>= map[y][x])continue;
+
+				//깎고, 방문처리함
 				map[cy][cx] -= k;
 				CNT++;
 				via[cy][cx] = 1;
@@ -45,8 +63,10 @@ void run(int y, int x)
 			}
 		}
 		
+		//이부분은 다음에 올 부분이 지금보다 작은 경우임
 		else {
 
+			//위의 방문처리와 동일
 			CNT++;
 			via[cy][cx] = 1;
 			run(cy, cx);
@@ -56,10 +76,13 @@ void run(int y, int x)
 
 	}
 
+	//최대등산로 갱신
 	if (maxcnt < CNT)
 		maxcnt = CNT;
 
 }
+
+//find 함수 : max값이 들어가있는 인덱스를 run 함수로 넘김.
 void find()
 {
 	int count = 0;
