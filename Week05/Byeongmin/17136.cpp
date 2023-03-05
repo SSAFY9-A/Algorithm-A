1cd7e453822c0218462fdaea230fe2a052994e05
bmryu0501 <bmryu0501@gmail.com>
2023-03-05 23:07:12 +0900

    solving boj 17136...

--- Week05/Byeongmin/17136.cpp
+++ Week05/Byeongmin/17136.cpp
@@ -1,42 +1,85 @@
 #include <iostream>
 
-#define SIZE 10
+#define SIZE 11
 
 using namespace std;
 
+int t;
+
 int papers[6] = {0, 5, 5, 5, 5, 5};
 bool arr[SIZE][SIZE];
-int answer = 25;
-int total;
+int presum[SIZE][SIZE];
+int answer = 26;
+int used, total;
+bool isAvailable = false;
 
 bool check(int y, int x, int size) {
-    for(int i=0;i<size;i++) for(int j=0;j<size;j++) {
-        if(!arr[y+i][x+j]) return false;
-    }
+    if(y+size > SIZE || x+size > SIZE) return false;
+    if(presum[y-1][x-1] - presum[y+size-1][x-1] - presum[y-1][x+size-1] + presum[y+size-1][x+size-1] != size * size) return false;
     return true;
 }
 
 void fill(int y, int x, int size, bool type) {
-    for(int i=0;i<size;i++) for(int j=0;j<size;j++) {
-        arr[y+i][x+j] = type;
+    int nx = x;
+    for(int i=0;i<size;i++) {
+        for(int j=0;j<size;j++) {
+            arr[y+i][x+j] = type;
+        }
+        nx = 1;
     }
 }
 
 void dfs(int y, int x) {
-    if(y == SIZE-1 && x == SIZE-1) return;
+    if(used >= answer) return;
 
-    
+    if(total == 0) { // 다 덮었으면
+        cout << "returning (" << y << ", " << x << ")\n";
+        isAvailable = true;
+        if(answer > used) answer = used;
+        return;
+    }
+
+    for(int i=y;i<SIZE;i++) {
+        for(int j=x;j<SIZE;j++) {
+            if(!arr[i][j]) continue;
+            for(int size=5;size>0;size--) {
+                if(!check(i, j, size)) continue;
+                if(!papers[size]) continue;
+
+                cout << "(" << i << ", " << j << ") " << size << '\n';
+                cin >> t;
+
+                papers[size]--;
+                fill(i, j, size, 0);
+                total -= size * size;
+                ++used;
+                dfs(i, j+size);
+                --used;
+                total += size * size;
+                fill(i, j, size, 1);
+                papers[size]++;
+                ++size;
+            }
+        }
+        x = 1;
+    }
 }
 
 int main() {
-    for(int i=0;i<SIZE;i++)for(int j=0;j<SIZE;j++) {
+    for(int i=1;i<SIZE;i++)for(int j=1;j<SIZE;j++) {
         cin >> arr[i][j];
-        if(arr[i][j]) total++;
+        presum[i][j] = arr[i][j];
+        if(arr[i][j]) ++total;
     }
 
-    dfs(0, 0);
+    // make prefix sum
+    for(int i=1;i<SIZE;i++)for(int j=1;j<SIZE;j++) {
+        presum[i][j] += presum[i-1][j] + presum[i][j-1] - presum[i-1][j-1];
+    }
 
-    if(answer <= 25) cout << answer;
+    dfs(1, 1);
+
+    if(isAvailable) cout << answer;
     else cout << -1;
 
     return 0;
